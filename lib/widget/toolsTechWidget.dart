c7f1ac01cf7bab66882114930bd05f82c2f750ba
siddhant <siddhantsaraf.91@gmail.com>
2022-02-19 23:48:04 +0530

    Initial commit

---
+++ lib/widget/toolsTechWidget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/provider/themeProvider.dart';
+import 'package:provider/provider.dart';
+
+import '../constants.dart';
+import 'adaptiveText.dart';
+
+class ToolTechWidget extends StatelessWidget {
+  final String techName;
+
+  const ToolTechWidget({Key key, this.techName}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final _themeProvider = Provider.of<ThemeProvider>(context);
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 12.0),
+      child: Row(
+        children: [
+          Icon(
+            Icons.play_arrow,
+            color: kPrimaryColor,
+            size: MediaQuery.of(context).size.height * 0.02,
+          ),
+          AdaptiveText(
+            " $techName ",
+            style: TextStyle(
+              color:
+                  _themeProvider.lightTheme ? Colors.grey[500] : Colors.white,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
